0ddb64a495b0d23de2f182aa7e68fbf8443ec79e
zerom-H <cupdisin@gmail.com>
2022-09-04 22:35:58 +0900

    image test

--- src/main/java/spring/lostark/LostarkApplication.java
+++ src/main/java/spring/lostark/LostarkApplication.java
@@ -16,17 +16,4 @@ public class LostarkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LostarkApplication.class, args);
 	}
-
-	@Bean
-	public HttpMessageConverter<String> responseBodyConverter() {
-		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
-	}
-
-	@Bean
-	public Filter characterEncodingFilter() {
-		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-		characterEncodingFilter.setEncoding("UTF-8");
-		characterEncodingFilter.setForceEncoding(true);
-		return characterEncodingFilter;
-	}
 }
