d5c05419c0d48e86602242e6bc5b088297ec6dc2
zerom-H <cupdisin@gmail.com>
2022-09-04 22:30:26 +0900

    image test

--- src/main/java/spring/lostark/LostarkApplication.java
+++ src/main/java/spring/lostark/LostarkApplication.java
@@ -2,6 +2,13 @@ package spring.lostark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class LostarkApplication {
@@ -10,4 +17,16 @@ public class LostarkApplication {
 		SpringApplication.run(LostarkApplication.class, args);
 	}
 
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter() {
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+	}
+
+	@Bean
+	public Filter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
 }
