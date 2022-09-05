021e144d72a687bfaf8f89b9529737ea19d957b0
zerom-H <cupdisin@gmail.com>
2022-09-05 23:23:35 +0900

    주석 추가 및 코드 정리

--- src/main/java/spring/lostark/config/WebConfig.java
+++ src/main/java/spring/lostark/config/WebConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 해당 설정만 해두면.. URI PATH 이하는 모두 로컬에서 파일이 가져와짐
+ * location = 로컬 경로 주소(실제 이미지가 있는 폴더 주소) pwd로 확인 가능
+ * uri_path = http uri 주소 나는 /upload로 해놨기에 http://localhost:8080/upload/** 이하 이미지가 인식됨
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
--- src/main/java/spring/lostark/config/utf8DecodeResourceResolver.java
+++ src/main/java/spring/lostark/config/utf8DecodeResourceResolver.java
@@ -10,7 +10,11 @@ import java.nio.charset.StandardCharsets;
 
 public class utf8DecodeResourceResolver extends PathResourceResolver implements ResourceResolver {
 
-
+    //해당 UTF_8이 되는지 재확인 필요
+    //맥 파일질라로 한글 파일 전송시 자음모음이 분리되는 현상이 있음..
+    //하지만 리눅스에서 파일조회 해보면 정상으로 나옴 ㅜㅜ..
+    //윈도우에서 확인하면 파일명 분리됨
+    //해당 Resolver 없이 되는지는 확인해볼 필요 있을듯..
     @Override
     protected Resource getResource(String resourcePath, Resource location) throws IOException {
         resourcePath = URLDecoder.decode(resourcePath, StandardCharsets.UTF_8);
